99109bbb76bf99a83f2d41ac007d3f9c98e49330
Tim Douglas <neurotech@gmail.com>
2019-08-28 23:42:58 +1000

    Basic functionality working

--- Boom.lua
+++ Boom.lua
@@ -1,55 +1,66 @@
 -- Thanks to:
 -- https://wow.gamepedia.com/API_CombatLogGetCurrentEventInfo
 
-local BOOM_ACTIVE = true
+-- TODO/IDEAS
+-- toggleBoom(active) function? (if active, run enable logic, else run disable logic)
+-- Threshold value via /boom n
+
+local BOOM_ACTIVE = false
+local BOOM_THRESHOLD = 20
+local BOOM_CHANNEL = "YELL"
+
 local playerGUID = UnitGUID("player")
 local MSG_CRITICAL_HIT = "#YOLO [ %s - %d ]"
 
--- enableBoom()
-function enableBoom()
-	-- TODO
-end
-
--- disableBoom()
-function disableBoom()
-	-- TODO
-end
-
 local f = CreateFrame("Frame")
-f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-f:SetScript("OnEvent", function(self, event)
-	self:OnEvent(event, CombatLogGetCurrentEventInfo())
-end)
 
-function f:OnEvent(event, ...)
-	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
-	local spellId, spellName, spellSchool
-	local amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand
-
-	if subevent == "SWING_DAMAGE" then
-		amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
-	elseif subevent == "SPELL_DAMAGE" then
-		spellId, spellName, spellSchool, amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
-	end
-	
-	if critical and sourceGUID == playerGUID then
-		local action = spellId and GetSpellLink(spellId) or "melee swing"
-		print(MSG_CRITICAL_HIT:format(action, amount))
-		-- Say it:
-		-- SendChatMessage(MSG_CRITICAL_HIT:format(action, amount) ,"SAY");
-		-- Yell it:
-		-- SendChatMessage(MSG_CRITICAL_HIT:format(action, amount) ,"YELL");
-	end
-end
-
--- Slash command to toggle Boom
-SLASH_BOOMTOGGLE = '/boom';
-function SlashCmdList.BOOMTOGGLE(msg, editbox)
-	BOOM_ACTIVE = not BOOM_ACTIVE
+function logStatus()
 	if BOOM_ACTIVE then
-		-- Call Enable()
+		print("|cFFFFFF00Boom is |cFF00FF00on|r.");
+		print("|cFF4EC6E0Boom!|r.");
 	else
-		-- Call Disable()
+		print("|cFFFFFF00Boom is |cFFFF0000off|r.");
 	end
-	print("Hello, World! "..BOOM_ACTIVE);
 end
+
+function toggleBoom(state)
+	if state then
+		BOOM_ACTIVE = false
+		f:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+		logStatus()
+	else
+		BOOM_ACTIVE = true
+		logStatus()
+		f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+		f:SetScript("OnEvent", function(self, event)
+			self:OnEvent(event, CombatLogGetCurrentEventInfo())
+		end)
+
+		function f:OnEvent(event, ...)
+			local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
+			local spellId, spellName, spellSchool
+			local amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand
+
+			if subevent == "SWING_DAMAGE" then
+				amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
+			elseif subevent == "SPELL_DAMAGE" then
+				spellId, spellName, spellSchool, amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
+			end
+			
+			if critical and sourceGUID == playerGUID then
+				if spellName then
+					-- Chat:
+					SendChatMessage(MSG_CRITICAL_HIT:format(spellName, amount), BOOM_CHANNEL)
+				end
+			end
+		end
+	end
+end
+
+SLASH_BOOMTOGGLE1 = "/boom"
+SlashCmdList["BOOMTOGGLE"] = function(msg, editBox)
+	toggleBoom(BOOM_ACTIVE)
+end
+
+print("|cFFFFFF00Boom loaded!|r")
+toggleBoom(BOOM_ACTIVE)
